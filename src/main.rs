ee88e3bd54af8f08ccf3007f616dc7211770a58a
Nicolas BAUW <nbauw@hotmail.com>
2020-06-04 23:15:22 +0200

    Splitting octal sextets

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-use std::io;
+use std::{io, str};
 
 fn main() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
 
     input.pop();
-    println!("{:?}", input.as_bytes());
+    let a = input.as_bytes();
+
+    println!("{:?}", a);
+    let octal = format!("{:o}", u32::from_be_bytes([0, a[0], a[1], a[2]]));
+    println!("{}", octal);
+
+    let sextets = octal.as_bytes()
+    .chunks(2)
+    .map(str::from_utf8)
+    .collect::<Result<Vec<&str>, _>>()
+    .unwrap();
+
+    println!("{:?}", sextets);
 }
