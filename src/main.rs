754c70b1294eb44755d4b173c206359429c82d81
Nicolas BAUW <nbauw@hotmail.com>
2020-06-05 15:21:21 +0200

    Converting octal output to a decimal index vector

--- src/main.rs
+++ src/main.rs
@@ -18,17 +18,24 @@ fn main() {
         0 => a.len(),
         _ => a.len() - (a.len() - a.len() % 3),
     };
+
+    // Creating octal output from bytes converted to sextets (3 * 8 = 24 bytes)
     while i < bytestoprocess {
         octal.push_str(format!("{:o}", u32::from_be_bytes([0, a[i], a[i + 1], a[i + 2]])).as_str());
         i += 3;
     }
     println!("{}", octal);
 
+    // Converting octal output to a decimal index vector
     let sextets = octal
         .as_bytes()
         .chunks(2)
         .map(str::from_utf8)
         .collect::<Result<Vec<&str>, _>>()
+        .unwrap()
+        .iter()
+        .map(|s| u8::from_str_radix(s, 8))
+        .collect::<Result<Vec<u8>, _>>()
         .unwrap();
 
     println!("{:?}", sextets);
