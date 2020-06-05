7b5a066a7766c14547d5a8bab902973447425d42
Nicolas BAUW <nbauw@hotmail.com>
2020-06-05 13:49:47 +0200

    Processing full input minus last incomplete sextet

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,28 @@ fn main() {
     let a = input.as_bytes();
 
     println!("{:?}", a);
-    let octal = format!("{:o}", u32::from_be_bytes([0, a[0], a[1], a[2]]));
+    let mut octal = String::new();
+
+    let mut i = 0;
+    println!("Length of string to encode : {}", a.len());
+
+    // The bytes to process without padding, generating a full sextets table
+    let bytestoprocess = match a.len() % 3 {
+        0 => a.len(),
+        _ => a.len() - (a.len() - a.len() % 3),
+    };
+    while i < bytestoprocess {
+        octal.push_str(format!("{:o}", u32::from_be_bytes([0, a[i], a[i + 1], a[i + 2]])).as_str());
+        i += 3;
+    }
     println!("{}", octal);
 
-    let sextets = octal.as_bytes()
-    .chunks(2)
-    .map(str::from_utf8)
-    .collect::<Result<Vec<&str>, _>>()
-    .unwrap();
+    let sextets = octal
+        .as_bytes()
+        .chunks(2)
+        .map(str::from_utf8)
+        .collect::<Result<Vec<&str>, _>>()
+        .unwrap();
 
     println!("{:?}", sextets);
 }
