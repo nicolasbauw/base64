0da170cc276757843543e5c44ae9362bd07ce620
Nicolas BAUW <nbauw@hotmail.com>
2020-06-10 15:41:37 +0200

    Can also decode (-d commutator)

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 use std::io;
 use lib_base64::Base64;
 
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "An alternative version of base64")]
+struct Opt {
+    #[structopt(short = "d")]
+    // We have to encore or decode ?
+    decode: bool
+}
+
 fn main() {
+    let opt = Opt::from_args();
     let mut input = String::new();
     if io::stdin().read_line(&mut input).is_err() == true { return };
 
     // removes line feed
     input.pop();
 
-    println!("{}", input.encode());
+    match opt.decode {
+        false => println!("{}", input.encode()),
+        true => println!("{}", input.decode())
+    }
 }
