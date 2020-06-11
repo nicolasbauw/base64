51aa9f89485900cbd5d492634131f571ec011b0f
Nicolas BAUW <nbauw@hotmail.com>
2020-06-11 11:46:37 +0200

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-use std::io;
 use lib_base64::Base64;
-
+use std::io;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 #[structopt(about = "An alternative version of base64")]
 struct Opt {
     #[structopt(short = "d")]
-    // We have to encore or decode ?
-    decode: bool
+    // We have to encode or decode ?
+    decode: bool,
 }
 
-fn main() {
+fn main() -> Result<(), lib_base64::Base64Error> {
     let opt = Opt::from_args();
     let mut input = String::new();
-    if io::stdin().read_line(&mut input).is_err() == true { return };
+    if io::stdin().read_line(&mut input).is_err() == true {
+        println!("Can't read stdin");
+        return Ok(());
+    };
 
     // removes line feed
     input.pop();
@@ -25,6 +27,7 @@ fn main() {
 
     match opt.decode {
         false => println!("{}", input.encode()),
-        true => println!("{}", input.decode())
+        true => println!("{}", input.decode()?),
     }
+    Ok(())
 }
